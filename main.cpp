b0ba4268a8b1a8a291b4905690b92e68b17b534e
Jordan-Linux <jor504@gmail.com>
2017-01-07 11:40:57 -0600

    Commit de prueba

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout<<"HOLA MUNDO";
+	}
+	
+
+	return 0;
+}
